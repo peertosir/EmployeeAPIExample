c346d828016943b39d8a3a00ef7869e3fe35dec4
Ilya Danilov <peertosir@gmail.com>
2021-04-09 13:48:41 +0300

    Sample DI test(context initialization)

--- src/test/java/dev/peertosir/payroll/PayrollApplicationTests.java
+++ src/test/java/dev/peertosir/payroll/PayrollApplicationTests.java
@@ -1,13 +1,24 @@
 package dev.peertosir.payroll;
 
+import dev.peertosir.payroll.controller.EmployeeController;
+import dev.peertosir.payroll.repository.EmployeeRepository;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PayrollApplicationTests {
 
+    @Autowired
+    private EmployeeController employeeController;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     @Test
     void contextLoads() {
+        assertThat(employeeController).isNotNull();
+        assertThat(employeeRepository).isNotNull();
     }
 
 }
